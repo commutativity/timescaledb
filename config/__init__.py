d041221779b226736e3d27c91e4a381d3befacf9
commutativity <95971893+commutativity@users.noreply.github.com>
2022-08-10 18:22:03 +0200

    first commit

---
+++ config/__init__.py
@@ -0,0 +1,2 @@
+from config import db_config
+from config.api_config import API_KEY, DB_HOST, DB_USER, DB_PASS, DB_NAME
---
+++ config/api_config.py
@@ -0,0 +1,8 @@
+API_URL = ''
+API_KEY = 'PES0ACSK92JYV6QB'
+API_SECRET = ''
+
+DB_HOST = 'localhost'
+DB_USER = 'postgres'
+DB_PASS = 'password'
+DB_NAME = 'etfdb'
